05143c1085f88146310399a6ae0b84fe3d6f27fa
username <xinjiewang@XinjiedeMacBook-Pro.local>
2024-01-30 09:21:05 -0500

    4th commit

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const map = document.getElementById('map');
 const SnakeTime = 200;
-let score= 0;
-let bestScore = localStorage.getItem('Bestscore') || 0;
-document.getElementById('Bestscore').innerText = 'Best Score: ' + bestScore;
+let timer;
+
+// create snake
+function Snake() {
+    // set snake height, width, and initial direction
+    this.width = 20;
+    this.height = 20;
+    this.direction = 'right';
+    this.body = [{ x: 2, y: 0, flag: null }];
+
+    // display snake
+    this.display = function () {
+        for (let i = 0; i < this.body.length; i++) {
+            if (this.body[i].x != null) {
+                const s = document.createElement('div');
+                this.body[i].flag = s;
+                s.style.width = this.width + 'px';
+                s.style.height = this.height + 'px';
+                s.style.position = 'absolute';
+                s.style.left = this.body[i].x * this.width + 'px';
+                s.style.top = this.body[i].y * this.height + 'px';
+                s.style.backgroundColor = 'yellow';
+                map.appendChild(s);
+            }
+        }
+        // set color of snake head as orange
+        this.body[0].flag.style.backgroundColor = 'orange';
+    };
+
+}
